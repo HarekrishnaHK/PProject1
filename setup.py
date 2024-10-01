1c50dab07d5827edfbee5d6960147a96aa57f438
Harekrishna <adhikaryharekrishna80@gmail.com>
2024-10-01 17:36:10 +0530

    setup

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ def get_requirements(file_path:str)->List[str]:
     requirements=[],
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
+        requirements=[req.replace("\n","")for req in requirements]
 
     setup (
     name ="PProject1",
